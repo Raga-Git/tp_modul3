1675eb1db84a2a526a51e79e2ae8852b9a455bca
Raga-Git <ragadhitya@gmail.com>
2024-03-23 23:29:15 +0700

    Menambahkan class SayaTubeVideo

--- tpmodul6_1302223025/Program.cs
+++ tpmodul6_1302223025/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public class random
+    {
+        public int idrandom()
+        {
+            Random id = new Random();
+            return id.Next(10000, 99999);
+        }
+    }
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playcount;
+
+        public SayaTubeVideo(string title)
+        {
+            random addId = new random();
+            this.title = title;
+            id = addId.idrandom();
+            playcount = 0;
+        }
+
+        public void increasePlaycount(int playcount)
+        {
+            this.playcount = playcount;
+            playcount++;
+        }
+        public void printVideoDetail()
+        {
+            Console.WriteLine($"id : {id}");
+            Console.WriteLine($"Judul : {this.title}");
+            Console.WriteLine($"Play Count : {this.playcount}");
+
+        }
+    }
+    private static void Main(string[] args)
+    {
+        SayaTubeVideo vid = new SayaTubeVideo("Tutorial Design By Contract - Ragadhitya Janatantra Koeshutama");
+        vid.increasePlaycount(1);
+        vid.printVideoDetail();
+    }
+}
